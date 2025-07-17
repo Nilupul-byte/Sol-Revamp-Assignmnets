5a72a1b7a3e2a5dcd86467d6a0f68d7acb8de8bd
Nilupul Weerasinghe <nilupulweerasinghe27@gmail.com>
2025-07-17 13:51:20 +0530

    Update VestingToken.sol

--- Module 04/VestingToken.sol
+++ Module 04/VestingToken.sol
@@ -1,1 +1,23 @@
+pragma solidity ^0.8.20;
 
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
+import "@openzeppelin/contracts/access/AccessControl.sol";
+
+contract VestingToken is ERC20, ERC20Burnable, AccessControl {
+    bytes32 public constant MINTER_ROLE = keccak256("MINTER_ROLE");
+    error ZeroAddress();
+
+    constructor(string memory name, string memory symbol, address admin, address vestingVault) 
+        ERC20(name, symbol) 
+    {
+        if (admin == address(0) || vestingVault == address(0)) revert ZeroAddress();
+        _mint(admin, 100_000_000 * 10**decimals());
+        _grantRole(DEFAULT_ADMIN_ROLE, admin);
+        _grantRole(MINTER_ROLE, vestingVault);
+    }
+
+    function mint(address to, uint256 amount) external onlyRole(MINTER_ROLE) {
+        _mint(to, amount);
+    }
+}
