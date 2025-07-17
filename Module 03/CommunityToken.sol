02153c455b5061aa8cec76927e2fbcba552cb11a
Nilupul Weerasinghe <nilupulweerasinghe27@gmail.com>
2025-07-17 11:32:02 +0530

    Update CommunityToken.sol

--- Module 03/CommunityToken.sol
+++ Module 03/CommunityToken.sol
@@ -1,1 +1,43 @@
+pragma solidity ^0.8.20;
 
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Pausable.sol";
+import "@openzeppelin/contracts/access/AccessControl.sol";
+
+contract CommunityToken is ERC20, ERC20Pausable, AccessControl {
+    bytes32 public constant MINTER_ROLE = keccak256("MINTER_ROLE");
+    bytes32 public constant PAUSER_ROLE = keccak256("PAUSER_ROLE");
+
+    error ZeroAddress();
+
+    constructor(string memory name, string memory symbol, address admin) 
+        ERC20(name, symbol) 
+    {
+        if (admin == address(0)) revert ZeroAddress();
+        _grantRole(DEFAULT_ADMIN_ROLE, admin);
+        _grantRole(PAUSER_ROLE, admin);
+    }
+
+    function mint(address to, uint256 amount) external onlyRole(MINTER_ROLE) {
+        _mint(to, amount);
+    }
+
+    function burn(uint256 amount) external {
+        _burn(msg.sender, amount);
+    }
+
+    function pause() external onlyRole(PAUSER_ROLE) {
+        _pause();
+    }
+
+    function unpause() external onlyRole(PAUSER_ROLE) {
+        _unpause();
+    }
+
+    function _update(address from, address to, uint256 amount) 
+        internal 
+        override(ERC20, ERC20Pausable) 
+    {
+        super._update(from, to, amount);
+    }
+}
